a74709867d9a24cacd468a24537f89689b2dd525
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-06 16:18:27 -0300

    adding getAllWithdraw to container ✅

--- src/@clean/shared/infra/containers/withdraw_container.ts
+++ src/@clean/shared/infra/containers/withdraw_container.ts
@@ -3,14 +3,14 @@ import "reflect-metadata";
 import { httpWithdraw } from "../http";
 import { WithdrawRepositoryMock } from "../repositories/withdraw_repository_mock";
 import { WithdrawRepositoryHttp } from "../repositories/withdraw_repository_http";
-import { CreateWithdrawUsecase } from "../../../modules/withdraw/usecases/create_withdraw_usecase";
+import { GetAllWithdrawUsecase } from "../../../modules/withdraw/usecases/get_all_withdraw_usecase"; // Importe o caso de uso GetAllWithdrawUsecase
 import { STAGE } from "../../domain/enums/stage_enum";
 
 export const RegistryWithdraw = {
   AxiosAdapter: Symbol.for("AxiosAdapter"),
   WithdrawRepositoryMock: Symbol.for("WithdrawRepositoryMock"),
   WithdrawRepositoryHttp: Symbol.for("WithdrawRepositoryHttp"),
-  CreateWithdrawUsecase: Symbol.for("CreateWithdrawUsecase"),
+  GetAllWithdrawUsecase: Symbol.for("GetAllWithdrawUsecase"),
 };
 
 export const containerWithdraw = new Container();
@@ -31,17 +31,17 @@ containerWithdraw
   });
 
 containerWithdraw
-  .bind(RegistryWithdraw.CreateWithdrawUsecase)
+  .bind(RegistryWithdraw.GetAllWithdrawUsecase)
   .toDynamicValue((context) => {
     if (process.env.EXPO_PUBLIC_STAGE === STAGE.TEST) {
-      return new CreateWithdrawUsecase(
+      return new GetAllWithdrawUsecase(
         context.container.get(RegistryWithdraw.WithdrawRepositoryMock)
       );
     } else if (
       process.env.EXPO_PUBLIC_STAGE === STAGE.PROD ||
       process.env.EXPO_PUBLIC_STAGE === STAGE.DEV
     ) {
-      return new CreateWithdrawUsecase(
+      return new GetAllWithdrawUsecase(
         context.container.get(RegistryWithdraw.WithdrawRepositoryHttp)
       );
     } else {
