5577241923a86ceb877d4860bf105f6bb28e7093
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-06 16:16:20 -0300

    adding getAllWithdraws() ✅

--- src/@clean/modules/withdraw/domain/repositories/withdraw_repository_interface.ts
+++ src/@clean/modules/withdraw/domain/repositories/withdraw_repository_interface.ts
@@ -1,10 +1,8 @@
-import { JsonWithdrawProps,  } from "../../../../shared/domain/entities/withdraw";
+import {
+  Withdraw,
+} from "../../../../shared/domain/entities/withdraw";
 
 export interface IWithdrawRepository {
-  createWithdraw(
-    notebookSerialNumber: string,
-    studentRA: string,
-    name: string,
-    initTime: number
-  ): Promise<JsonWithdrawProps>;
+
+  getAllWithdraws(): Promise<Withdraw[]>;
 }
